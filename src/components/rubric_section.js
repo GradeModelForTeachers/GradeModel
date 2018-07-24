e60d086b8c22817c5f4f249e9623c89aa88fe1cc
wdsand <willds40@gmail.com>
2018-07-24 19:44:02 -0400

    WDS - refactors rubric sections but thesis changes both assetion and thesis scores

--- src/components/rubric_section.js
+++ src/components/rubric_section.js
@@ -22,93 +22,35 @@ export default class RubricSection extends Component {
     return scores;
   }
 
+  displaySEctionBoxesAndInputs(){
+    let sectionBoxesAndInput = []
+    let iterator = this.props.score.keys()
+    for (var score of this.props.score) {
+      for (let key of iterator) {
+        let keyAsString = key.toString();
+      sectionBoxesAndInput.push(<td className='rubric-number rubric-section'>{key}
+      <input checked={this.state.selectedOption === keyAsString}
+       className='radio-section-button' type="radio" value={keyAsString}
+       onChange={this.props.handleTextAndScoreChange}
+       onClick={this.handleSelectRadioButton.bind(this)}
+       value={Object.values(score)}
+       title={this.props.section}
+       id={keyAsString}
+       />
+      </td>
+    )}
+
+  }
+    return sectionBoxesAndInput
+  }
+
   render() {
     return (
       <div className="rubric-container">
         <table className="col-md-12 rubric">
         <tr>
-        <td className='rubric-number rubric-section'>1
-        <input checked={this.state.selectedOption === '1'}
-         className='radio-section-button' type="radio" value="1"
-        onChange={this.props.handleTextAndScoreChange}
-         onClick={this.handleSelectRadioButton.bind(this)}
-         value={Object.values(this.props.score[0])}
-         title={this.props.section}
-         id="1"
-         />
-        </td>
-        <td className='rubric-number rubric-section'> 2
-        <input checked={this.state.selectedOption === '2'}
-         onClick={this.handleSelectRadioButton.bind(this)}
-         onChange={this.props.handleTextAndScoreChange}
-         className='radio-section-button' type="radio" value="2"
-         value={Object.values(this.props.score[1])}
-         title={this.props.section}
-        id="2"
-         />
-        </td>
-        <td className='rubric-number rubric-section'> 3
-        <input checked={this.state.selectedOption === '3'}
-         onClick={this.handleSelectRadioButton.bind(this)}
-         onChange={this.props.handleTextAndScoreChange}
-         className='radio-section-button' type="radio" value="3"
-         value={Object.values(this.props.score[2])}
-         title={this.props.section}
-        id="3"
-         />
-        </td>
-        <td className='rubric-number rubric-section'> 4
-        <input checked={this.state.selectedOption === '4'}
-         onClick={this.handleSelectRadioButton.bind(this)}
-         onChange={this.props.handleTextAndScoreChange}
-         className='radio-section-button' type="radio" value="4"
-         value={Object.values(this.props.score[3])}
-         title={this.props.section}
-        id="4"
-         />
-        </td>
-        <td className='rubric-number rubric-section'> 5
-        <input checked={this.state.selectedOption === '5'}
-         onClick={this.handleSelectRadioButton.bind(this)}
-         onChange={this.props.handleTextAndScoreChange}
-         className='radio-section-button' type="radio" value="5"
-         value={Object.values(this.props.score[4])}
-         title={this.props.section}
-        id="5"
-         />
-        </td>
-        <td className='rubric-number rubric-section'> 6
-        <input checked={this.state.selectedOption === '6'}
-         onClick={this.handleSelectRadioButton.bind(this)}
-         onChange={this.props.handleTextAndScoreChange}
-         className='radio-section-button' type="radio" value="6"
-         value={Object.values(this.props.score[5])}
-         title={this.props.section}
-        id="6"
-         />
-        </td>
-        <td className='rubric-number rubric-section'> 7
-        <input checked={this.state.selectedOption === '7'}
-         onClick={this.handleSelectRadioButton.bind(this)}
-         onChange={this.props.handleTextAndScoreChange}
-         className='radio-section-button' type="radio" value="7"
-         value={Object.values(this.props.score[6])}
-         title={this.props.section}
-        id="7"
-         />
-        </td>
-        <td className='rubric-number rubric-section'> 8
-        <input checked={this.state.selectedOption === '8'}
-         onClick={this.handleSelectRadioButton.bind(this)}
-         onChange={this.props.handleTextAndScoreChange}
-         className='radio-section-button' type="radio" value="8"
-         value={Object.values(this.props.score[7])}
-         title={this.props.section}
-        id="8"
-         />
-        </td>
+        {this.displaySEctionBoxesAndInputs()}
         </tr>
-
         <tr>
         {this.displayRubricScoreAndCriteria()}
         </tr>
