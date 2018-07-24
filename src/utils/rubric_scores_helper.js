00e661b818523892d120b213f7784f21ac73269b
wdsand <willds40@gmail.com>
2018-07-24 18:45:04 -0400

    mend

---
+++ src/utils/rubric_scores_helper.js
@@ -0,0 +1,4 @@
+export var getSectionScores = (sectionTtile, rubric,) => {
+  const section = rubric.filter(sectionItem=> sectionItem.section_title === sectionTtile)
+  return section[0].section_scores
+}
