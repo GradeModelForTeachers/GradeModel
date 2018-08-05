75499a0402df91efba01a851ab419f2c913ef7c7
nhelvig <nhelvig@thoughtworks.com>
2018-08-05 01:48:12 -0700

    [Nick] Fixing linting

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,50 +1,63 @@
 import React, { Component } from 'react';
-import Essay from "./essay";
+import Essay from './essay';
 import Rubric from './rubric';
 import Comments from './comments';
 import Summary from './summary';
-import Modal from './modal'
+import Modal from './modal';
 
-export default class Homepage extends Component {
+class Homepage extends Component {
   constructor() {
     super();
     this.state = {
-    open: false,
-    commmentTypeBar:false,
-    commentType:"",
-    }
-    this.onToggleSummary = this.onToggleSummary.bind(this)
-    this.onSubmitGrade = this.onSubmitGrade.bind(this)
-  }
-  onSubmitGrade(){
-    window.alert("Thank You For Submitting Your Grade");
+      open: false,
+      commmentTypeBar: false,
+      commentType: '',
+    };
+    this.onToggleSummary = this.onToggleSummary.bind(this);
+    this.onSubmitGrade = this.onSubmitGrade.bind(this);
   }
 
-  onToggleSummary = () => {
+  onSubmitGrade() {
+    window.alert('Thank You For Submitting Your Grade');
+  }
+
+  onToggleSummary() {
     this.setState({ open: !this.state.open });
-  };
+  }
 
   render() {
     return (
       <div>
         <Rubric />
-        {this.state.open ===  false ?
-          <span className='add-comment col-md-7 '>
-            <button onClick={this.onToggleSummary} className='summary-button'>View Summary</button>
-          </span> :
-          <span className='add-comment col-md-7 '>
-            <button onClick={this.onToggleSummary} className='summary-button'>Close Summary</button>
-          </span>
+        {this.state.open === false
+          ? (
+            <span className="add-comment col-md-7 ">
+              <button onClick={this.onToggleSummary} className="summary-button">
+View Summary
+              </button>
+            </span>
+          )
+          : (
+            <span className="add-comment col-md-7 ">
+              <button onClick={this.onToggleSummary} className="summary-button">
+Close Summary
+              </button>
+            </span>
+          )
         }
-        {this.state.open ===  false ? null :
-          <Summary />
+        {this.state.open === false ? null
+          : <Summary />
         }
         <Essay />
-        <Comments/>
-        <div className='submit-essay col-md-8'>
-          <button onClick={this.onSubmitGrade} className='submit-button'>Submit Grade</button>
+        <Comments />
+        <div className="submit-essay col-md-8">
+          <button onClick={this.onSubmitGrade} className="submit-button">
+Submit Grade
+          </button>
         </div>
       </div>
     );
   }
 }
+
+export default Homepage;
