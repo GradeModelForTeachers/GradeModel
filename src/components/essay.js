456ed64f1220757e30df08bb1507c5b8e4118877
wdsand <willds40@gmail.com>
2018-09-08 11:36:15 -0400

    WDS componetizes css for comment homepage

--- src/components/essay.js
+++ src/components/essay.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchEssay } from '../actions/index';
-
+import '../styles/essay.css';
 
 export class Essay extends Component {
   componentDidMount() {
