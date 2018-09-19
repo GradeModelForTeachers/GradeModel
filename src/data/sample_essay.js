59ae6bbea0e8b84d4d7421af75a196b1a9210a87
angelicaperez37 <angelica_perez@gap.com>
2018-09-19 08:38:19 -0700

    [Angelica] Updates sample essay and bag of words test

--- src/data/sample_essay.js
+++ src/data/sample_essay.js
@@ -3,5 +3,5 @@
 //   [  // TODO: for this array, should we ever expect more than one object?
 export default {
   scores: { thesis: 5, assertion: 7 },
-  essay_text: 'essay text text',
+  essay_text: 'essay text text essay text essay essay',
 };
--- src/tests/data/essay.spec.js
+++ src/tests/data/essay.spec.js
@@ -3,5 +3,5 @@ import sampleEssay from '../../data/sample_essay.js';
 
 test('should build bag of words with correct counts', () => {
   const essay = Essay.fromEssayObject(sampleEssay);
-  expect(essay.bagOfWords).toEqual({'essay': 1, 'text': 2});
+  expect(essay.bagOfWords).toEqual({'essay': 4, 'text': 3});
 });
